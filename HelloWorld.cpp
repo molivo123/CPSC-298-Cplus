757c177ca7a2549b8b7352798b3a5ee5e7aefc3c
root <molivo@chapman.edu>
2020-02-11 19:10:52 +0000

    I just changed my Hello world from interterm

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main (){
   cout << "Hello World";
   cout << "i had to add this cuz prof said so" << endl;
-  
+  cout << "I have now done this for interterm and 350" << endl;
+
   return 0;
 }
